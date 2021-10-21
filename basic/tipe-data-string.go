32fa2b032e0df1122ddd1bef711677cb43a3dac7
zenklot <zenklot11@gmail.com>
2021-10-21 09:56:56 +0700

    writing with string

---
+++ basic/tipe-data-string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Raisa")
+	fmt.Println("Supriatna")
+	fmt.Println("Raisa Supriatna")
+	fmt.Println("Gozenx")
+	fmt.Println("Zenklot")
+	fmt.Println("Ambil Karakter kedua"[1])
+	fmt.Println(len("Hitung jumlah karakter"))
+}
