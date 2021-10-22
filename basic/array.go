02dc36cfb01e6365854ae6d5684f8352a9e7ca93
zenklot <zenklot11@gmail.com>
2021-10-22 10:16:28 +0700

    Writing code with array

---
+++ basic/array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+	names[0] = "Raisa"
+	names[1] = "Supriatna"
+	names[2] = "Raisa Supriatna"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+}
