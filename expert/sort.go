48d215155c51cef3edee2547b129157cec8f728f
zenklot <zenklot11@gmail.com>
2021-10-25 10:18:06 +0700

    writing code with package sort

---
+++ expert/sort.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (userSlice UserSlice) Len() int {
+	return len(userSlice)
+}
+
+func (userSlice UserSlice) Less(i, j int) bool {
+	return userSlice[i].Age < userSlice[j].Age
+}
+
+func (userSlice UserSlice) Swap(i, j int) {
+	userSlice[i], userSlice[j] = userSlice[j], userSlice[i]
+}
+
+func main() {
+	users := []User{
+		{"Supriatna", 23},
+		{"Raisa", 12},
+
+		{"zenklot", 22},
+		{"Gozenx", 22},
+	}
+
+	sort.Sort(UserSlice(users))
+
+	fmt.Println(users)
+}
